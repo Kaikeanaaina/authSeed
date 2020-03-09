cc8275eb561bd0291529091dbe37e36611be34b2
Kaikeanaaina <kaikeanaaina@gmail.com>
2020-03-09 10:50:27 -1000

    Delete dev.js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID:'756704747306-tuc9sf7kn0k1h5358v3ruk0em2q6nt6t.apps.googleusercontent.com',
-    googleClientSecret: 'C_X2JcD3E0NxunsQ9WKiTGo_',
-    mongoURI:'mongodb+srv://kaikeanaaina:CJ7Xnwv6jZI3gyxM@authseed-y62tf.mongodb.net/test?retryWrites=true&w=majority',
-    atlasSecret: 'CJ7Xnwv6jZI3gyxM',
-    cookieKey: 'aksldfahsdkgjhadfiguhasd;klfjas;dufhapdisufhas;kfjads;puighapiduhgas;dkjfasdoifjhsal;kdpiuhvpeirubihjmsa;lkdfmnjkchsdui'
-  }
