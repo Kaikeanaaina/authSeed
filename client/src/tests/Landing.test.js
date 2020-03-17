eab4fa05d118d48b9e459b681d5c394644101669
KainoaKeanaaina <kaikeanaaina@gmail.com>
2020-03-16 22:15:34 -1000

    added more describe

--- client/src/tests/Landing.test.js
+++ client/src/tests/Landing.test.js
@@ -1,8 +1,33 @@
+//when writing code for test, this is what to brainstorm
+// what tags rendering are you expecting?
+// what props/states rendering are you expecting?
+
+//integration tests than unit tests
+
+
 import React from 'react'
 import { mount } from 'enzyme'
 import Landing from '../components/Landing'
 
-//const props = { note: {text: 'text note'}}
+//this is called a spread attributes
+//spread operator
+const props = { note: {text: 'text note'}}
+
+console.log({...props})
+
+// ===============================
+const triplePrint= (a,b,c,) => {
+    console.log(`${a} ${b} ${c}`)
+}
+const message =[ 'react', 'is', 'awesome']
+
+//an example why spread operators are helpful,
+// it could cleanly show everything in the array with more simple code
+triplePrint('react', 'is', 'awesome')
+triplePrint(message[0], message[1], message[2])
+triplePrint(...message)
+
+// =================================
 
 describe('Landing', () => {
     let landing = mount(<Landing />)
