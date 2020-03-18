07fab3208d4f17749c0fa8bea4b52d824a02313f
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-18 00:10:38 -1000

    example

---
+++ config/exampleDev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:'your google client ID here.apps.googleusercontent.com',
+    googleClientSecret: 'your google Client Secret Code here',
+    mongoURI:'mongodb+srv://USER:<password>@<sandboxID>.mongodb.net/test?retryWrites=true&w=majority',
+    atlasSecret: 'your atlas Secret Code Here',
+    cookieKey: 'your secret cookie key here'
+  }
